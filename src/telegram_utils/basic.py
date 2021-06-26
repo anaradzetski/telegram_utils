6af8d7c52a88c489d90d270a972b3aded4e6b510
Andrey Naradzetski <andrey.naradzetski@gmail.com>
2021-06-26 17:46:42 +0300

    added reshape_sequence

--- src/telegram_utils/basic.py
+++ src/telegram_utils/basic.py
@@ -1,6 +1,6 @@
-import re
-from typing import Callable
-from functools import wraps
+"""Some basic utils ofently used in telegram projects"""
+from typing import Callable, Sequence, List
+from functools import wraps, singledispatch
 
 from telegram import Update, ChatAction
 from telegram.ext import Updater, CallbackContext
@@ -8,7 +8,7 @@ from telegram.ext import Updater, CallbackContext
 def get_updater(path_to_token: str) -> Updater:
     """Get Updater by path to file with token"""
     with open(path_to_token) as f:
-        return Updater(re.match(r'\S+', f.read()).group(0))
+        return Updater(f.read().strip())
 
 
 def typing(obj: Callable) -> Callable:
@@ -25,7 +25,7 @@ def typing(obj: Callable) -> Callable:
 
 
 def make_message_sender(send_message_kwargs) -> Callable:
-    """Makes callback context which sends message on the effect"""
+    """Makes callback function which sends message on the effect"""
     @typing
     def message_sender(update: Update, context: CallbackContext):
         context.bot.send_message(
@@ -33,3 +33,36 @@ def make_message_sender(send_message_kwargs) -> Callable:
             **send_message_kwargs
         )
     return message_sender
+
+
+@singledispatch
+def reshape_sequence(sequence: Sequence, shape) -> List:
+    """Used mostly for keyboard creating"""
+    raise ValueError('Invalid shape parameter.')
+
+
+@reshape_sequence.register
+def _(sequence: Sequence, shape: int) -> List:
+    if shape <= 0:
+        raise ValueError('Only positive shape is acceptable.')
+    return [sequence[i: i + shape] for i in range(0, len(sequence), shape)]
+
+
+@reshape_sequence.register
+def _(sequence: Sequence, shape: Sequence) -> List:
+    ret = []
+    cur_start = 0
+    try:
+        for row_len, idx in enumerate(shape):
+            if row_len == -1:
+                if idx != len(shape) -1:
+                    raise ValueError('-1 not at the end of shape array.')
+                ret.append(sequence[cur_start, -1])
+            elif row_len > 0:
+                ret.append(sequence[cur_start: row_len])
+                cur_start += row_len
+            else:
+                raise ValueError('Invalid row length.')
+    except KeyError as ex:
+        raise ValueError('Invalid shape array.') from ex
+    return ret
